a9be75f098a1e1f4013c804371f611448749e2b8
Etudiant <BRIKIbesma>
2023-09-03 14:36:51 +0200

    exercice1:charger une image binaire 16 bits en niveau gris

--- exercice1.py
+++ exercice1.py
@@ -3,15 +3,16 @@ import numpy as np
 
 # Charger une image binaire de ce format
 IMG_HEIGHT = 480
+
 IMG_WIDTH = 640
 CHANNEL = 1 # Grayscale
 
 with open("resources/thermal_img_640x480_16bits_grayscale.bin", 'rb') as file:
     buffer = file.read()
 
-# Astuce une image est une matrice (numpy est une librairie interressante quand il est question de matrice)
-# https://numpy.org/doc/stable/reference/arrays.ndarray.html
-thermal_img = 
+# Convertir le buffer en une image numpy
+thermal_img = np.frombuffer(buffer, dtype=np.uint16).reshape((IMG_HEIGHT, IMG_WIDTH, CHANNEL))
+ 
 
 
 # Faites une normalisation MIN/MAX sur 8 bits de l'image (utiliser opencv)
