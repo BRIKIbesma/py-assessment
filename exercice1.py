e837fed3a2ed2afd5e04ff3a176e256091bb47c0
Etudiant <BRIKIbesma>
2023-09-03 14:52:50 +0200

    exercice1:  normaliser l'image thermal_img sur 8 bits (0-255) en utilisant une normalisation MIN/MAX

--- exercice1.py
+++ exercice1.py
@@ -12,7 +12,14 @@ with open("resources/thermal_img_640x480_16bits_grayscale.bin", 'rb') as file:
 
 # Convertir le buffer en une image numpy
 thermal_img = np.frombuffer(buffer, dtype=np.uint16).reshape((IMG_HEIGHT, IMG_WIDTH, CHANNEL))
- 
+
+# Faire une normalisation MIN/MAX sur 8 bits de l'image 
+normalized_img = cv2.normalize(thermal_img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+# Faites une normalisation MIN/MAX sur 8 bits de l'image (utilisez OpenCV)
+min_val = np.min(thermal_img)
+max_val = np.max(thermal_img)
+normalized_img = ((thermal_img - min_val) / (max_val - min_val) * 255).astype(np.uint8) 
 
 
 # Faites une normalisation MIN/MAX sur 8 bits de l'image (utiliser opencv)
